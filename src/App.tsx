854a7a9b3b7b2cd4026ccec1a52eb7a9a82bcb54
Yasamin <yaasamin.amini@gmail.com>
2024-09-03 14:46:56 +0200

    Made list group component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Message from './Message'
+import ListGroup from "./Components/ListGroup"; 
 //Creating a new component called App
 function App(){
   // We want a message in our div component
-  return <div><Message /></div>
+  return <div><ListGroup /></div>
 }
 
 export default App;
