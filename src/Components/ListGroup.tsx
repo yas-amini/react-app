a56ef287b33a1f9326bf22a019b609abbb63bddb
Yasamin <yaasamin.amini@gmail.com>
2024-09-04 12:30:06 +0200

    Rendered lists

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,18 @@
 // Defines a functional component named 'ListGroup'
 function ListGroup() {
-    return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
-    );
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // Transform the items array into a list of <li> elements
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        ;
+      </ul>
+    </>
+  );
 }
 
-export default ListGroup; 
+export default ListGroup;
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,8 @@
 // PascalCasing
 function Message() {
-// The syntax is called JSX: JavaScript XML
-const name = 'Yas'
-return <h1>Hello {name}</h1>;
+    // The syntax is called JSX: JavaScript XML
+    const name = 'Yas'
+    return <h1>Hello {name}</h1>;
 }
 
 export default Message;
