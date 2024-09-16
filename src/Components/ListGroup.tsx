d07f63f6191d337458efda452940e6a7430029d2
Yasamin <yaasamin.amini@gmail.com>
2024-09-16 14:21:00 +0200

    Conditional rendering

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,15 +1,17 @@
 // Defines a functional component named 'ListGroup'
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
   // Transform the items array into a list of <li> elements
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
-        ;
       </ul>
     </>
   );
