98933cb7fd604d8f06c0f57739e1beeb6ebf5988
Yasamin <yaasamin.amini@gmail.com>
2024-09-16 14:34:18 +0200

    Type annotation

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,16 +1,23 @@
 // Defines a functional component named 'ListGroup'
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
+  const handleClick = (event: MouseEvent) => console.log(event);
   // Transform the items array into a list of <li> elements
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
