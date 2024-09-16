01455868a2eac170ba1b4b30c17c0a46767e847a
Yasamin <yaasamin.amini@gmail.com>
2024-09-16 14:35:55 +0200

    Handling events

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -3,6 +3,7 @@ import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  // Event handler
   const handleClick = (event: MouseEvent) => console.log(event);
   // Transform the items array into a list of <li> elements
   return (
@@ -11,11 +12,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
